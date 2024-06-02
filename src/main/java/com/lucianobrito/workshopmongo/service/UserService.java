bd2d84b1a7d7adc1b28c4fc41b5b1d06f2d2ba40
Luciano Brito <lucianobrsts@yahoo.com.br>
2024-06-02 10:35:59 -0300

    Atualização de usuário com PUT

--- src/main/java/com/lucianobrito/workshopmongo/service/UserService.java
+++ src/main/java/com/lucianobrito/workshopmongo/service/UserService.java
@@ -29,10 +29,22 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = userRepository.findById(obj.getId()).get();
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
 	}
 
 	public User fromDTO(UserDTO userDTO) {
