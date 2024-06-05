f3eba12f9ff19a636c5a3f9e1908d1d035415995
Luciano Brito <lucianobrsts@yahoo.com.br>
2024-06-05 00:06:51 -0300

    Consulta com vários critérios

--- src/main/java/com/lucianobrito/workshopmongo/resources/PostResource.java
+++ src/main/java/com/lucianobrito/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.lucianobrito.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,28 +18,40 @@ import com.lucianobrito.workshopmongo.service.PostService;
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
-	
+
 	@Autowired
 	private PostService postService;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Post>> findAll() {
 		List<Post> posts = postService.findAll();
 		return ResponseEntity.ok().body(posts);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
 	}
-	
+
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = postService.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
-	
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 
 }
