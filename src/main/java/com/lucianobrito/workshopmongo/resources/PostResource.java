d313d1e9727be048a1dbe5e18df7d3c24b9a8a34
Luciano Brito <lucianobrsts@yahoo.com.br>
2024-06-04 23:13:29 -0300

    Consulta simples com query methods

--- src/main/java/com/lucianobrito/workshopmongo/resources/PostResource.java
+++ src/main/java/com/lucianobrito/workshopmongo/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucianobrito.workshopmongo.domain.Post;
+import com.lucianobrito.workshopmongo.resources.util.URL;
 import com.lucianobrito.workshopmongo.service.PostService;
 
 @RestController
@@ -30,5 +32,13 @@ public class PostResource {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
 	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 
 }
